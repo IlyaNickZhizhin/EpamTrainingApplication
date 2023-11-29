f05a98222e627b167d54c32511a280f4e7a8b63b
Ilya Zhizhin <ilyanikzhizhin@gmail.com>
2023-11-29 15:44:34 +0500

    Hibernate task finished remark https://github.com/IlyaNickZhizhin/EpamTrainingApplication/pull/4#discussion_r1407551094 resolved

--- src/test/java/org/epam/ReaderTest.java
+++ src/test/java/org/epam/ReaderTest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class TestingReader {
+public class ReaderTest {
     @Test
     public void testReadEntity() {
         Reader reader = new Reader();
