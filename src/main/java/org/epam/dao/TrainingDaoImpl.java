3fca4d2f5b30a8d2707756c80cc0eb58ff60e4a0
Ilya Zhizhin <ilyanikzhizhin@gmail.com>
2023-11-30 16:20:59 +0500

    Hibernate task finished remark https://github.com/IlyaNickZhizhin/EpamTrainingApplication/pull/4#discussion_r1410424869 about possible NPE resolved.

--- src/main/java/org/epam/dao/TrainingDaoImpl.java
+++ src/main/java/org/epam/dao/TrainingDaoImpl.java
@@ -1,7 +1,6 @@
 package org.epam.dao;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.collections4.CollectionUtils;
 import org.epam.exceptions.InvalidDataException;
 import org.epam.exceptions.ProhibitedActionException;
 import org.epam.model.gymModel.Trainee;
@@ -11,7 +10,6 @@ import org.epam.model.gymModel.TrainingType;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.engine.jdbc.StreamUtils;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
