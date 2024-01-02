913fcc31df5dd3c9ec0fd2f0ab8a35a516b7bf5c
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-01-02 15:09:51 +0500

    Logic of WorkloadService was change according to the task

--- ReportService/src/main/java/org/epam/reportservice/service/WorkloadService.java
+++ ReportService/src/main/java/org/epam/reportservice/service/WorkloadService.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.epam.reportservice.dto.TrainerWorkloadRequest;
 import org.epam.reportservice.dto.TrainerWorkloadResponse;
 import org.epam.reportservice.mapper.ReportMapper;
+import org.epam.reportservice.model.ActionType;
 import org.epam.reportservice.repository.WorkloadStorage;
 import org.springframework.stereotype.Service;
 
@@ -20,6 +21,29 @@ public class WorkloadService {
     private final ReportMapper mapper;
 
     public TrainerWorkloadResponse change(TrainerWorkloadRequest request) {
+        return request.getActionType().equals(ActionType.ADD) ? addWorkload(request) : deleteWorkload(request);
+    }
+
+    private TrainerWorkloadResponse deleteWorkload(TrainerWorkloadRequest request) {
+        TrainerWorkloadResponse response = mapper.RequestToResponse(request);
+        if (storage.getStorage().get(request.getUsername()) == null) return response;
+        if (storage.getStorage().get(request.getUsername())
+                .get(Year.of(request.getTrainingDate().getYear())) == null) return response;
+        if (storage.getStorage().get(request.getUsername())
+                .get(Year.of(request.getTrainingDate().getYear()))
+                .get(request.getTrainingDate().getMonth()) == null) return response;
+        double duration = storage.getStorage().get(request.getUsername())
+                .get(Year.of(request.getTrainingDate().getYear()))
+                .get(request.getTrainingDate().getMonth());
+        duration -= request.getDuration();
+        storage.getStorage().get(request.getUsername())
+                .get(Year.of(request.getTrainingDate().getYear()))
+                .put(request.getTrainingDate().getMonth(), duration);
+        response.setMap(storage.getStorage().get(request.getUsername()));
+        return response;
+    }
+
+    private TrainerWorkloadResponse addWorkload(TrainerWorkloadRequest request) {
         Month month = request.getTrainingDate().getMonth();
         Year year = Year.of(request.getTrainingDate().getYear());
         Map<Year, Map<Month, Double>> history = storage.getStorage()
