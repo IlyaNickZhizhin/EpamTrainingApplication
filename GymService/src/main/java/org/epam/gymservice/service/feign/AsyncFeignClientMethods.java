6c1b5a16ded987409ba0f0d6b3db1e6bdfb63796
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-01-08 14:24:18 +0500

    first circuit breaker realization

--- GymService/src/main/java/org/epam/gymservice/service/feign/AsyncFeignClientMethods.java
+++ GymService/src/main/java/org/epam/gymservice/service/feign/AsyncFeignClientMethods.java
@@ -1,12 +1,15 @@
 package org.epam.gymservice.service.feign;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
 import org.epam.gymservice.config.ReportFeignClient;
 import org.epam.gymservice.dto.reportDto.TrainerWorkloadRequest;
 import org.epam.gymservice.dto.reportDto.TrainerWorkloadResponse;
+import org.epam.gymservice.dto.reportDto.TrainingSession;
 import org.epam.gymservice.dto.trainerDto.TrainerProfileResponse;
 import org.epam.gymservice.dto.trainingDto.AddTrainingRequest;
 import org.epam.gymservice.dto.trainingDto.GetTraineeTrainingsListRequest;
+import org.epam.gymservice.dto.trainingDto.GetTrainerTrainingsListRequest;
 import org.epam.gymservice.dto.trainingDto.TrainingDto;
 import org.epam.gymservice.service.TraineeService;
 import org.epam.gymservice.service.TrainerService;
@@ -15,6 +18,8 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -25,6 +30,7 @@ public class AsyncFeignClientMethods {
     private final TraineeService traineeService;
 
     @Async
+    @HystrixCommand(fallbackMethod = "fallbackWorkloadChange")
     public CompletableFuture<TrainerWorkloadResponse> addWorkload(String bearerToken, AddTrainingRequest request) {
         TrainerProfileResponse trainer = trainerService.selectByUsername(request.getTrainerUsername());
         return CompletableFuture
@@ -43,8 +49,17 @@ public class AsyncFeignClientMethods {
     }
 
     @Async
+    @HystrixCommand(fallbackMethod = "fallbackWorkloadChange")
     public CompletableFuture<TrainerWorkloadResponse> deleteWorkload(String bearerToken, TrainerWorkloadRequest request) {
         return CompletableFuture.supplyAsync(() -> feignClient.deleteWorkload(bearerToken, request));
+    }
+
+    public CompletableFuture<TrainerWorkloadResponse> fallbackWorkloadChange(String bearerToken, AddTrainingRequest request){
+        TrainerProfileResponse trainer = trainerService.selectByUsername(request.getTrainerUsername());
+        List<TrainingDto> trainings = trainerService.getTrainerTrainingsList(request.getTrainerUsername(), new GetTrainerTrainingsListRequest()).getTrainings();
+        Queue<TrainingSession> sessions = new PriorityQueue<>();
+        sessions.addAll(trainings.stream().map(training -> TrainingSession.of(training.getTrainingDate(), training.getDuration())).toList());
+        return CompletableFuture.completedFuture(TrainerWorkloadResponse.of(trainer, request.getTrainerUsername(), sessions));
     }
 
 }
