92a9b19ed9d7ba10f22e0841fa882f0c7e8943f5
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-01-11 15:23:11 +0500

    Change histrix on spring-cloud-starter-circutbreaker

--- GymService/src/main/java/org/epam/gymservice/config/feign/ReportFeignClient.java
+++ GymService/src/main/java/org/epam/gymservice/config/feign/ReportFeignClient.java
@@ -1,14 +1,16 @@
-package org.epam.gymservice.config;
+package org.epam.gymservice.config.feign;
 
 import org.epam.gymservice.dto.reportDto.TrainerWorkloadRequest;
 import org.epam.gymservice.dto.reportDto.TrainerWorkloadResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.context.annotation.Primary;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "report-service")
+@FeignClient(value = "report-service", fallback = ReportFeignClientFallback.class)
+@Primary
 public interface ReportFeignClient {
 
     @PostMapping("/v1/api/workload/change")
--- GymService/src/main/java/org/epam/gymservice/service/feign/AsyncFeignClient.java
+++ GymService/src/main/java/org/epam/gymservice/service/feign/AsyncFeignClient.java
@@ -1,15 +1,12 @@
 package org.epam.gymservice.service.feign;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import lombok.RequiredArgsConstructor;
-import org.epam.gymservice.config.ReportFeignClient;
+import org.epam.gymservice.config.feign.ReportFeignClient;
 import org.epam.gymservice.dto.reportDto.TrainerWorkloadRequest;
 import org.epam.gymservice.dto.reportDto.TrainerWorkloadResponse;
-import org.epam.gymservice.dto.reportDto.TrainingSession;
 import org.epam.gymservice.dto.trainerDto.TrainerProfileResponse;
 import org.epam.gymservice.dto.trainingDto.AddTrainingRequest;
 import org.epam.gymservice.dto.trainingDto.GetTraineeTrainingsListRequest;
-import org.epam.gymservice.dto.trainingDto.GetTrainerTrainingsListRequest;
 import org.epam.gymservice.dto.trainingDto.TrainingDto;
 import org.epam.gymservice.service.TraineeService;
 import org.epam.gymservice.service.TrainerService;
@@ -18,8 +15,6 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -30,7 +25,6 @@ public class AsyncFeignClient {
     private final TraineeService traineeService;
 
     @Async
-    @HystrixCommand(fallbackMethod = "takeWorkloadFromGymService")
     public CompletableFuture<TrainerWorkloadResponse> addWorkload(String bearerToken, AddTrainingRequest request) {
         TrainerProfileResponse trainer = trainerService.selectByUsername(request.getTrainerUsername());
         return CompletableFuture
@@ -49,17 +43,8 @@ public class AsyncFeignClient {
     }
 
     @Async
-    @HystrixCommand(fallbackMethod = "takeWorkloadFromGymService")
     public CompletableFuture<TrainerWorkloadResponse> deleteWorkload(String bearerToken, TrainerWorkloadRequest request) {
         return CompletableFuture.supplyAsync(() -> feignClient.deleteWorkload(bearerToken, request));
-    }
-
-    public CompletableFuture<TrainerWorkloadResponse> takeWorkloadFromGymService(String bearerToken, AddTrainingRequest request){
-        TrainerProfileResponse trainer = trainerService.selectByUsername(request.getTrainerUsername());
-        List<TrainingDto> trainings = trainerService.getTrainerTrainingsList(request.getTrainerUsername(), new GetTrainerTrainingsListRequest()).getTrainings();
-        Queue<TrainingSession> sessions = new PriorityQueue<>();
-        sessions.addAll(trainings.stream().map(training -> TrainingSession.of(training.getTrainingDate(), training.getDuration())).toList());
-        return CompletableFuture.completedFuture(TrainerWorkloadResponse.of(trainer, request.getTrainerUsername(), sessions));
     }
 
 }
