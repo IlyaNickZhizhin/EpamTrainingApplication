2e4c12edd54992ac79b564150fb4a80b96944afd
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-01-11 15:23:11 +0500

    Change histrix on spring-cloud-starter-circutbreaker

--- GymService/src/main/java/org/epam/gymservice/GymServiceApplication.java
+++ GymService/src/main/java/org/epam/gymservice/GymServiceApplication.java
@@ -2,12 +2,10 @@ package org.epam.gymservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableHystrix
 public class GymServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(GymServiceApplication.class, args);
