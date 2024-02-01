be1d1d843b7f123980954881bf782b28609d42eb
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-02-01 16:18:09 +0500

    dead letter queue implemented

--- ReportService/src/main/java/org/epam/reportservice/controller/WorkloadReceiver.java
+++ ReportService/src/main/java/org/epam/reportservice/controller/WorkloadReceiver.java
@@ -8,6 +8,7 @@ import org.epam.reportservice.service.WorkloadService;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 @RequiredArgsConstructor
@@ -17,6 +18,7 @@ public class WorkloadReceiver {
     private final WorkloadService workloadService;
     private final JmsTemplate jmsTemplate;
 
+    @Transactional
     @JmsListener(destination = "addTrainingRequestQueue", containerFactory = "defaultJmsListenerContainerFactory")
     public TrainerWorkloadResponse receiveAddMessage(TrainerWorkloadRequest request){
         log.info("Adding workload of trainer{} {}***", request.getFirstName(), request.getLastName().charAt(0));
@@ -28,6 +30,7 @@ public class WorkloadReceiver {
         }
     }
 
+    @Transactional
     @JmsListener(destination = "deleteTrainingRequestQueue", containerFactory = "defaultJmsListenerContainerFactory")
     public TrainerWorkloadResponse receiveDeleteMessage(TrainerWorkloadRequest request){
         log.info("Deleting workload of trainer{} {}***", request.getFirstName(), request.getLastName().charAt(0));
