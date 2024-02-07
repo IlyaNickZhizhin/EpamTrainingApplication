77cb82f0184300ad1e4239699d01268ab142e124
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-02-07 14:44:52 +0500

    App was changed for sending JSON dto instead of objects.

--- GymService/src/main/java/org/epam/gymservice/config/JmsConfig.java
+++ GymService/src/main/java/org/epam/gymservice/config/JmsConfig.java
@@ -1,22 +1,27 @@
 package org.epam.gymservice.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.RedeliveryPolicy;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
+
+import java.text.SimpleDateFormat;
 
 @Configuration
-@PropertySource("classpath:secretStore.yaml")
 public class JmsConfig {
-
-    @Value("${AMQ.url}")
+    @Value("${activemq.broker-url}")
     private String brokerUrl;
-    @Value("${AMQ.login}")
+    @Value("${activemq.user}")
     private String username;
-    @Value("${AMQ.password}")
+    @Value("${activemq.password}")
     private String password;
 
     @Bean
@@ -32,6 +37,21 @@ public class JmsConfig {
 
     @Bean
     public JmsTemplate jmsTemplate(){
-        return new JmsTemplate(getActiveMqConnectionFactory());
+        JmsTemplate template = new JmsTemplate(getActiveMqConnectionFactory());
+        template.setMessageConverter(jacksonConverter());
+        return template;
+    }
+
+    @Bean
+    public MessageConverter jacksonConverter(){
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd"));
+        converter.setObjectMapper(mapper);
+        return converter;
     }
 }
