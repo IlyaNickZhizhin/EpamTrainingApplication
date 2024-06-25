d717b88d636d0ac1351821a70cc9030a30af40ec
ilyaZhizhin <ilyanikzhizhin@gmail.com>
2024-06-25 16:39:14 +0500

    hallo controller added (try8)

--- GymService/src/main/java/org/epam/gymservice/controller/HalloController.java
+++ GymService/src/main/java/org/epam/gymservice/controller/HalloController.java
@@ -13,7 +13,7 @@ public class HalloController {
                 "<html>" +
                         "<body>" +
                         "<p>Frontend part has not been finished,</p>" +
-                        "<p>please use swagger UI on this <a href=\"http://64.225.100.214:2052/swagger-ui/index.html\">link</a></p>" +
+                        "<p>please use swagger UI on this <a href=\"https://ilya.zhizhin.xyz/swagger-ui/index.html\">link</a></p>" +
                         "<p>1) enter /v3/api-docs in the Swagger UI page</p>" +
                         "<p>2) push \"Explore\" button</p>" +
                         "<p>3) explore it</p>" +
